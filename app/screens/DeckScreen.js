52142834a65882ae726bd19738637aaa215c916e
uncleoptimus <uncle.optimus@gmail.com>
2017-11-16 18:43:49 -0800

    Tweak Deck Screen question list styling; Incorporate answer hide/show toggle for  listed questions

--- app/screens/DeckScreen.js
+++ app/screens/DeckScreen.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native'
 
+import { Ionicons } from '@expo/vector-icons'
+
+
 //  ☐ displays an option to start a quiz on this specific deck
 //  ☐ An option to add a new question to the deck
 class DeckScreen extends Component {
@@ -9,12 +12,42 @@ class DeckScreen extends Component {
     const { deck, navigation } = this.props
 
     // const { title } = navigation.state.params
-    const QuestionItem = ({ item: question }) => (
-      <View style={{ margin: 10 }}>
-        <Text>Question: {question.question}</Text>
-        <Text>Answer: {question.answer}</Text>
-      </View>
-    )
+    // const QuestionItem = ({ item: question }) => (
+
+    class AccordionItem extends Component {
+      state = {
+        open: false,
+      }
+
+      toggleOpen = () => {
+        this.setState((state) => ({ open: !state.open }))
+      }
+
+      render() {
+        const { question } = this.props
+        const { open } = this.state
+
+        return (
+          <View style={{ margin: 10 }}>
+            <TouchableOpacity style={styles.accordion_row} onPress={this.toggleOpen}>
+              <Ionicons
+                name={'md-arrow-dropdown'}
+                size={32}
+                style={styles.icon_button}
+              />
+              <Text style={styles.question_text}>
+                Q: {question.question}
+              </Text>
+            </TouchableOpacity>
+            {(open) &&
+              <Text style={styles.answer_text}>{question.answer}</Text>
+            }
+          </View>
+        )
+      }
+    }
+
+    const QuestionItem = ({ item: question }) => <AccordionItem question={question} />
 
     return (
       <View style={styles.container}>
@@ -36,14 +69,32 @@ class DeckScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    padding: 15,
   },
   header_text: {
     fontSize: 20,
+    fontWeight: '600',
   },
   header_sub: {
     fontSize: 16,
-  }
+    marginBottom: 10,
+  },
+  question_text: {
+    fontSize: 18,
+  },
+  answer_text: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    marginLeft: 20,
+  },
+  accordion_row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  icon_button: {
+    // padding: 5,
+    marginRight: 10,
+  },
 })
 
 const mapStateToProps = (state, props) => {
